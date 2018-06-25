24cb3967a1ef5ccbb3638d25a7d9953caf54755d
lss8 <38145232+lss8@users.noreply.github.com>
2018-06-24 22:49:42 -0300

    Add files via upload

--- negocio/CadastroProduto.java
+++ negocio/CadastroProduto.java
@@ -6,20 +6,16 @@ import exceptions.*;
 
 public class CadastroProduto {
 	private RepositorioProduto produtos ;
-	
+
 	public CadastroProduto(char tipo) {
 		if ((tipo == 'a') || (tipo == 'A')) {
 			produtos = new RepositorioProdutoArray()  ;
 		}
 		else {
 			produtos = new RepositorioProdutoLista() ;
-		}	
+		}
 	}
-	
-	public CadastroProduto(RepositorioProdutoLista rpl) {
-		
-	}
-	
+
 	public void cadastrar(Produto produto) throws ProdutoJaCadastradoException {
 		if (!produtos.existe(produto.getTipo())) {
 			produtos.inserir(produto);
@@ -28,15 +24,15 @@ public class CadastroProduto {
 			throw new ProdutoJaCadastradoException() ;
 		}
 	}
-	
+
 	public void remover(String tipo) throws ProdutoNaoEncontradoException {
 		produtos.remover(tipo);
 	}
-	
+
 	public void atualizar(Produto produto) throws ProdutoNaoEncontradoException {
 		produtos.atualizar(produto);
 	}
-	
+
 	public Produto procurar(String tipo) throws ProdutoNaoEncontradoException {
 		return produtos.procurar(tipo) ;
 	}
